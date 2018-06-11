c929fbc6a0592e0da85c0474beaf4aecbaeb5db5
Javaフォルダ構成アップします <>
2018-06-11 16:12:46 +0900

    占いサービス

--- CampChallenge_java/009.サーブレット/FortuneTelling.java
+++ CampChallenge_java/009.サーブレット/FortuneTelling.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package org.mypackage.sample;
+package Test;
+
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -17,36 +13,20 @@ import java.util.Random;
  *
  * @author hojo
  */
-public class FortuneTelling1 extends HttpServlet {
+public class FortuneTelling extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+   
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         
+        //占い結果
         String[] luckList = {"大吉", "中吉", "吉", "半吉", "末小吉", "凶", "小凶", "半凶", "末凶", "凶", "大凶"};
         Random rand = new Random();
-//乱数所得
+        
+        //乱数所得
         Integer index = rand.nextInt(luckList.length);
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet FortuneTelling1</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet FortuneTelling1 at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
             out.print(luckList[index]);
         }
     }
@@ -88,5 +68,4 @@ public class FortuneTelling1 extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
 }
