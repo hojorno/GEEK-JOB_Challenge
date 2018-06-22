c5aa2bed4aeaaf94f9a14adb11a74a113d936983
Hojorno Giovanna <>
2018-06-22 17:07:29 +0900

    タイムスタンプ

--- CampChallenge_java/015.標準クラス/Timestamp.java
+++ CampChallenge_java/015.標準クラス/Timestamp.java
@@ -13,10 +13,9 @@ public class Timestamp
     public static void main(String[] args) 
     {
         Calendar cl = Calendar.getInstance();
-        Date date = new Date();
         
         cl.set(2016, 0, 1, 0, 0, 0);
-        date = cl.getTime();
+        Date date = cl.getTime();
         
         System.out.println(date);
         System.out.println(date.getTime());
