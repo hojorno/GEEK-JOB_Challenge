aedd76ab297d05359ead355c2b3ee0d90eaaa558
Hojorno Giovanna <>
2018-06-25 13:24:47 +0900

    バイト数と文字数

---
+++ CampChallenge_java/015.標準クラス/Byte.java
@@ -0,0 +1,16 @@
+package Standardclass;
+
+/**
+ *
+ * @author hojornogiovanna
+ */
+public class Byte 
+{
+    public static void main(String[] args) 
+    {
+        String name = "HojornoGiovanna";
+    
+        System.out.println(name.getBytes().length);
+        System.out.println(name.length());
+    }
+}
