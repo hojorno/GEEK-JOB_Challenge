ca0fec39f0bec36711a246b8913da72e39851f3a
Hojorno Giovanna <>
2018-06-25 14:28:56 +0900

    文字の入れ替え

---
+++ CampChallenge_java/015.標準クラス/chikan.java
@@ -0,0 +1,18 @@
+package Standardclass;
+
+/**
+ *
+ * @author hojornogiovanna
+ */
+public class chikan 
+{
+    public static void main(String[] args) 
+    {
+        String nikki ="きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+        
+        nikki = nikki.replace("U", "う");
+        nikki = nikki.replace("I", "い");
+        
+        System.out.println(nikki);
+    }
+}
