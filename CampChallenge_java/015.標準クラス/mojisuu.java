ea838fb6c06c095debb6582e296d63b9e1e117ed
Hojorno Giovanna <>
2018-06-25 14:10:49 +0900

    文字数の取得

---
+++ CampChallenge_java/015.標準クラス/mojisuu.java
@@ -0,0 +1,16 @@
+package Standardclass;
+
+/**
+ *
+ * @author hojornogiovanna
+ */
+public class mojisuu 
+{
+    public static void main(String[] args) 
+    {
+        String address ="HojornoGivanna@GoldExperience.com";
+        String add = address.substring(15);
+        
+        System.out.println(add.length());
+    }
+}
