91b62f76caffa47baae8d246bba5852179fdbc56
Hojorno Giovanna <>
2018-07-03 13:13:32 +0900

    セッションの作成

---
+++ CampChallenge_java/016.データ操作/SESSION.java
@@ -0,0 +1,79 @@
+package Datamanipulation;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author hojornogiovanna
+ */
+public class SESSION extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            HttpSession hs = request.getSession();
+            
+            if (hs.getAttribute("Data") != null) {
+                String Date = (String) hs.getAttribute("Data");
+                out.print("前回のログインは:"+Date);
+            }
+            
+            Date now = new Date();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy,MM,dd HH:mm:ss");
+            hs.setAttribute("Data", sdf.format(now));
+            
+            out.print("　現在:"+sdf.format(now));
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
